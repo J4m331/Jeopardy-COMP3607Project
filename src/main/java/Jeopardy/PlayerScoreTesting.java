405be01cf257664a8f62e385c37c9b55dcf1b8f9
Matthew-Moodoo <matthew.moodoo@my.uwi.edu>
2025-11-23 15:37:35 -0400

    Create initial testing file for answer options.

--- src/main/java/Jeopardy/PlayerScoreTesting.java
+++ src/main/java/Jeopardy/PlayerScoreTesting.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
 
-class PlayerTesting{
+class PlayerScoreTesting{
     private Player player;
 
     @BeforeEach
